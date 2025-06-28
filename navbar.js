92c75f3449c6c4da453a771b352b859f2bc62462
KaelidoscopeGG <carlmichael209@gmail.com>
2025-06-28 19:57:42 +0800

    Add files via upload
    nav bar stuff

---
+++ navbar.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("ThriftTreeSidenav").style.width = "275px";
+}
+
+function closeNav() {
+    document.getElementById("ThriftTreeSidenav").style.width = "0";
+}
