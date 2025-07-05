4be212ab61bae42845cb4a787bb57cd6838702e7
KaelidoscopeGG <carlmichael209@gmail.com>
2025-07-05 16:29:12 +0800

    Update navbar.js
    the dreaded js for charts oh my god i have to do 2 more charts pls kill me

--- navbar.js
+++ navbar.js
@@ -5,3 +5,51 @@ function openNav() {
 function closeNav() {
     document.getElementById("site_sidenav").style.width = "0";
 }
+
+// pie chart 
+
+// line chart
+const lineChart = new Chart("line-chart", {
+  type: "line",
+  data: {},
+  options: {}
+});
+
+// bar chart
+let barLabels = ['Bills', 'Food', 'School', 'Rent', 'Wants'];
+let barData = [5500, 3000, 1000, 3500, 2500];
+
+const barChartData = {
+  labels: barLabels,
+  datasets: [{
+    label: 'Expenses',
+    data: barData,
+    backgroundColor: 'rgba(255, 162, 0, 1)',
+    textColor: 'rgb(255,255,255)',
+    borderColor: 'rgb(255, 162, 0)',
+    borderWidth: 1
+  }]
+};
+
+const barCanvas = document.getElementById('bar-chart');
+if (barCanvas) {
+  new Chart(barCanvas, {
+    type: 'bar',
+    data: barChartData,
+    options: {
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+      plugins: {
+        legend:{
+          display: false
+        }
+      }
+    }
+  });
+} else {
+  console.error("Canvas with ID 'bar-chart' not found.");
+}
